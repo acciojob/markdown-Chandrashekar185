cf916505734749ff5b3f0fbe11f27b765c735767
Chandrashekar185 <chanduminny32@gmail.com>
2025-08-05 15:52:14 +0530

    Create MarkdownEditor.jsx

---
+++ src/components/MarkdownEditor.jsx
@@ -0,0 +1,29 @@
+
+import React, { useEffect, useState } from "react";
+import { marked } from "marked";
+
+const MarkdownEditor = ({ markdown, setMarkdown }) => {
+  const [preview, setPreview] = useState("");
+
+  useEffect(() => {
+    setPreview(marked(markdown));
+  }, [markdown]);
+
+  return (
+    <div className="editor-container">
+      <textarea
+        className="textarea"
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
+        placeholder="Write your markdown here..."
+      />
+
+      <div
+        className="preview"
+        dangerouslySetInnerHTML={{ __html: preview }}
+      />
+    </div>
+  );
+};
+
+export default MarkdownEditor;
